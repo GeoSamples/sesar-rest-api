682d8f21e579b4536a99dacf6f5d24679671dea7
lulin-song <lulin.song@gmail.com>
2019-05-09 16:21:59 -0400

    Updated comments.

--- src/main/java/org/earthchem/sesarrestapi/controller/CountryController.java
+++ src/main/java/org/earthchem/sesarrestapi/controller/CountryController.java
@@ -1,6 +1,5 @@
 package org.earthchem.sesarrestapi.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.earthchem.sesarrestapi.dao.CountryDAO;
--- src/main/java/org/earthchem/sesarrestapi/service/CountryService.java
+++ src/main/java/org/earthchem/sesarrestapi/service/CountryService.java
@@ -1,6 +1,5 @@
 package org.earthchem.sesarrestapi.service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
 

280f5d410a42d8f119580121d60e5f2c1bbeb1c0
lulin-song <lulin.song@gmail.com>
2019-05-13 14:51:36 -0400

    Fixed class type issue.

--- src/main/java/org/earthchem/sesarrestapi/repository/SesarUserCodeRoleRepo.java
+++ src/main/java/org/earthchem/sesarrestapi/repository/SesarUserCodeRoleRepo.java
@@ -1,9 +1,9 @@
 package org.earthchem.sesarrestapi.repository;
 
-import org.earthchem.sesarrestapi.model.Classification;
+import org.earthchem.sesarrestapi.model.SesarUserCodeRole;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SesarUserCodeRoleRepo extends CrudRepository<Classification, Integer> {
+public interface SesarUserCodeRoleRepo extends CrudRepository<SesarUserCodeRole, Integer> {
 	}

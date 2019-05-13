63a8911158ab7e654602d36a49f7f32a49944a85
lulin-song <lulin.song@gmail.com>
2019-05-13 17:10:52 -0400

    Improve platform type REST API by using type name alias mapping.

--- src/main/java/org/earthchem/sesarrestapi/controller/SampleController.java
+++ src/main/java/org/earthchem/sesarrestapi/controller/SampleController.java
@@ -1,19 +1,29 @@
 package org.earthchem.sesarrestapi.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.earthchem.sesarrestapi.model.Sample;
 import org.earthchem.sesarrestapi.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -45,11 +55,36 @@ public class SampleController {
 	@GetMapping(path="/samples/platformtypes", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)  
 	@ResponseBody
 	public ResponseEntity<List<String> > getPlatformTypes() {  
-		return new ResponseEntity<List<String> >(service.getPlatformTypes(), HttpStatus.OK);
+		List<String> l = service.getPlatformTypes();
+		List<String> nl = new ArrayList<String>();
+		try {
+          //  File file = ResourceUtils.getFile("volcabury_mapping/platform_type_mapping.json");
+            //InputStream in = new FileInputStream(file);
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/platform_type_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+			System.out.println(pl);
+			
+
+		    for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s)) s2 = pl.get(s);
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }
+        
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
 	}
 	
 	@ApiOperation(value = "Get the existing list of physionic features from SESAR")
-	@GetMapping(path="/samples/physionicfeatures", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)  
+	@GetMapping(path="/samples/physiogeographicfeatures", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)  
 	@ResponseBody
 	public ResponseEntity<List<String> > getPhysionicFeatures() {  
 		return new ResponseEntity<List<String> >(service.getPrimaryLocationTypes(), HttpStatus.OK);

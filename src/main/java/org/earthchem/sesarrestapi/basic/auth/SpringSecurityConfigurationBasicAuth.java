55adcfcb7bec34f9700ae77eda99815c6e946652
lulin-song <lulin.song@gmail.com>
2019-06-25 12:06:02 -0400

    Allow anonymouse authentication to REST APIs

---
+++ src/main/java/org/earthchem/sesarrestapi/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,20 @@
+package org.earthchem.sesarrestapi.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+         http
+         .csrf().disable()
+         .authorizeRequests()
+         .anyRequest().anonymous().and().httpBasic().disable();        
+    }
+}

b28392e4ca966b013df27d48e82d450794c7a753
lulin-song <5776339+lulin-song@users.noreply.github.com>
2021-01-22 15:57:11 -0500

    Reorganized URIs. Create three new controller to hold similar feature URIs. They will show up in three different section in Swagger documents.

---
+++ src/main/java/org/earthchem/sesarrestapi/controller/StatisticsController.java
@@ -0,0 +1,98 @@
+package org.earthchem.sesarrestapi.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+
+import org.earthchem.sesarrestapi.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:8080")
+@RequestMapping("/v1")
+@Api(value="Statistics", description="SESAR Statistics API",  tags = { "Statistics" })
+public class StatisticsController {
+	@Autowired
+	private SampleService service;
+    
+    @ApiOperation(value = "Get published IGSN total number")
+    @GetMapping(path="/igsns/publishedtotal", produces = MediaType.APPLICATION_JSON_VALUE)  
+    @ResponseBody
+    public ResponseEntity<Integer> getAllPublishedIGSNTotalNumber() 
+    {  
+    	Integer l = service.getAllPublishedIGSNTotalNumber();      
+	    return new ResponseEntity<Integer>(l, HttpStatus.OK);
+    }
+  
+    @ApiOperation(value = "Get published top level IGSN total number")
+    @GetMapping(path="/rootigsns/publishedtotal", produces = MediaType.APPLICATION_JSON_VALUE)  
+    @ResponseBody
+    public ResponseEntity<Integer> getAllPublishedParentIGSNTotalNumber() 
+    {  
+    	Integer l = service.getAllPublishedParentIGSNTotalNumber();      
+	    return new ResponseEntity<Integer>(l, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Get registered IGSN count by institution and country between start date and end date. If the end date is omitted, current date will be the end date. If hideprivate is 1, unpublished IGSNs will not be counted.")
+    @GetMapping(path= {"/igsns/count/institution"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<LinkedHashMap<String, String> > getIGSNCountByInstitude(@ApiParam(value = "Registration date in a format of YYYY-MM-DD", required=true) @RequestParam(required = true) String start_date,
+                                                                      @RequestParam(required = true) @ApiParam(value = "Registration date in a format of YYYY-MM-DD",required=true) String end_date,
+                                                                      @RequestParam(required = false) @ApiParam(value = "If 1, sample with unpublished metadata will be excluded in the count.") Integer hideprivate)
+    {
+        LinkedHashMap<String,String> a = service.getIGSNCountByInstitude(start_date, end_date, hideprivate);
+        if(a == null)  return new ResponseEntity<LinkedHashMap<String,String> >(new LinkedHashMap<String,String>(), 
+		                                                                 HttpStatus.NOT_FOUND);
+        if(a.size() == 0 )
+        {
+            return new ResponseEntity<LinkedHashMap<String,String> >(a, HttpStatus.NOT_FOUND);
+        }
+        else if(a.size() == 1 )
+        {
+            if(a.get("error") == null)
+                return new ResponseEntity<LinkedHashMap<String,String> >(a, HttpStatus.BAD_REQUEST);
+            //System.err.println(a.toString());
+        }
+        return new ResponseEntity<LinkedHashMap<String,String> >(a, HttpStatus.OK);
+    }
+	
+    @ApiOperation(value = "Get registered IGSN count by sample type from inception up to end date. If the end date is omitted, current date will be the end date. If hideprivate is 1, unpublished IGSNs will not be counted.")
+    @GetMapping(path= {"/igsns/count/sampletype"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<LinkedHashMap<String, String> > getIGSNCountBySampleType(@RequestParam(required = false) @ApiParam(value = "Registration date: end date. E.G. YYYY-MM-DD") String end_date,
+                                                                                   @RequestParam(required = false) @ApiParam(value = "If 1, sample with unpublished metadata will be excluded.") Integer hideprivate)
+    {
+    	String edate=null;
+    	if(end_date == null)
+    	{
+    		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		    edate = dateFormat.format(new Date());	
+    	}
+    	else edate = end_date;
+        LinkedHashMap<String,String> a = service.getIGSNCountBySampleType(edate, hideprivate);
+        if(a == null)  return new ResponseEntity<LinkedHashMap<String,String> >(new LinkedHashMap<String,String>(), 
+		                                                                 HttpStatus.NOT_FOUND);
+        if(a.size() == 0 )
+        {
+            return new ResponseEntity<LinkedHashMap<String,String> >(a, HttpStatus.NOT_FOUND);
+        }
+        else if(a.size() == 1 )
+        {
+            if(a.get("error") == null)
+                return new ResponseEntity<LinkedHashMap<String,String> >(a, HttpStatus.BAD_REQUEST);
+            //System.err.println(a.toString());
+        }
+        return new ResponseEntity<LinkedHashMap<String,String> >(a, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/org/earthchem/sesarrestapi/controller/UserVocabController.java
@@ -0,0 +1,217 @@
+package org.earthchem.sesarrestapi.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.earthchem.sesarrestapi.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/v1")
+@Api(value="UserVocab", description="Collected Vocaburaries from User in SESAR",  tags = { "UserVocab" })
+public class UserVocabController {
+	@Autowired
+	private SampleService service;
+
+	@ApiOperation(value = "Get existing list of platform type from SESAR")
+	@GetMapping(path="/vocabularies/platformtypes", produces = MediaType.APPLICATION_JSON_VALUE)  
+	@ResponseBody
+	public ResponseEntity<List<String> > getPlatformTypes() {  
+		List<String> l = service.getPlatformTypes();
+		List<String> nl = new ArrayList<String>();
+		try {
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/platform_type_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+			System.out.println(pl);
+			
+
+		    for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s)) 
+		  	    {
+		  	    	if(pl.get(s).length()==0) continue;
+		  	    	s2 = pl.get(s);
+		  	    }
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }    
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Get the existing list of collection  methods from SESAR")
+	@GetMapping(path="/vocabularies/collectionmethods", produces = MediaType.APPLICATION_JSON_VALUE)  
+	@ResponseBody
+	public ResponseEntity<List<String> > getCollectionMethods() { 
+		
+		List<String> l = service.getCollectionMethods();
+		List<String> nl = new ArrayList<String>();
+		try {
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/collection_method_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+		    for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s)) 
+		  	    {
+		  	    	if(pl.get(s).length()==0) continue;
+		  	    	s2 = pl.get(s);
+		  	    }
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Get the existing list of collector from SESAR")
+	@GetMapping(path="/vocabularies/collectors", produces = MediaType.APPLICATION_JSON_VALUE)  
+	@ResponseBody
+	public ResponseEntity<List<String> > getCollectors() { 
+		
+		List<String> l = service.getCollectors();
+		List<String> nl = new ArrayList<String>();
+		try {
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/collector_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+		    for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s)) 
+		  	    {
+		  	    	if(pl.get(s).length()==0) continue;
+		  	    	s2 = pl.get(s);
+		  	    }
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
+	}	
+
+	@ApiOperation(value = "Get existing list of platform name from SESAR")
+	@GetMapping(path="/vocabularies/platformnames", produces = MediaType.APPLICATION_JSON_VALUE)  
+	@ResponseBody
+	public ResponseEntity<List<String> > getPlatformNames() {  
+		List<String> l = service.getPlatformNames();
+		List<String> nl = new ArrayList<String>();
+		try {
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/platform_name_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+		    for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s))
+		  	    {
+		  	    	if(pl.get(s).length()==0) continue;
+		  	    	s2 = pl.get(s);
+		  	    }
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }  
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Get existing list of field name from SESAR")
+	@GetMapping(path="/vocabularies/cruisefieldprograms", produces = MediaType.APPLICATION_JSON_VALUE)  
+	@ResponseBody
+	public ResponseEntity<List<String> > getFieldNames() {  
+		List<String> l = service.getCruiseFieldPrgrm();
+		List<String> nl = new ArrayList<String>();
+		try {
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/cruise_field_program_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+			for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s)) 
+		  	    {
+		  	    	if(pl.get(s).length()==0) continue; 
+		  	    	s2 = pl.get(s);
+		  	    }
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }       
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Get the existing list of physionic features from SESAR")
+	@GetMapping(path="/vocabularies/physiogeographicfeatures", produces = MediaType.APPLICATION_JSON_VALUE)  
+	@ResponseBody
+	public ResponseEntity<List<String> > getPhysiographicFeatures() { 
+		List<String> l = service.getPrimaryLocationTypes();
+		List<String> nl = new ArrayList<String>();
+		try {
+            InputStream in = new ClassPathResource(
+            	      "volcabulary_mapping/physiographic_feature_mapping.json").getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference< HashMap<String, String> > typeReference = new TypeReference< HashMap<String,String> >(){};
+			HashMap<String,String> pl = mapper.readValue(in,typeReference);
+		    for( String s: l)
+		    {
+		    	String s2;
+		  	    if(pl.containsKey(s)) 
+		  	    {
+		  	    	if(pl.get(s).length()==0) continue;
+		  	    	s2 = pl.get(s);
+		  	    }
+		  	    else s2 = s;
+			    if(nl.contains(s2)) continue;
+			    nl.add(s2);
+		    }
+        } catch (IOException e) {
+           return new ResponseEntity<List<String> >(new ArrayList<String>(), HttpStatus.NOT_FOUND);
+        }	
+		return new ResponseEntity<List<String> >(nl, HttpStatus.OK);
+	}
+}

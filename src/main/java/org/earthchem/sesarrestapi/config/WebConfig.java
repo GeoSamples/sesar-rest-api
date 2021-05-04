be2b9918caee30500ff2d73987e9247e582ddc4d
lulin-song <5776339+lulin-song@users.noreply.github.com>
2021-05-04 17:37:30 -0400

    Removed EnableWebMvc annotation in WebConfig.java which caused swagger-ui.html mapping issue.

--- src/main/java/org/earthchem/sesarrestapi/config/WebConfig.java
+++ src/main/java/org/earthchem/sesarrestapi/config/WebConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
-//@ComponentScan ({ "org.earthchem.sesarrestapi" })
 @ComponentScan
 public class WebConfig implements WebMvcConfigurer {
 
